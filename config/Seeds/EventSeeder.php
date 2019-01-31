1ff994461310bfdc1a8f6c856124c717e5918a90
Hoang Duc Quan <rocker.gryphon@gmail.com>
2019-01-31 11:05:08 +0700

    setup events table database

---
+++ config/Seeds/EventSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class EventSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'foo',
+                'description' => 'Quan rat dep trai',
+                'event_date' => date('Y-m-d H:i:s'),
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'bar',
+                'description' => 'Quan rat dep trai',
+                'event_date' => date('Y-m-d H:i:s'),
+                'created' => date('Y-m-d H:i:s'),
+                'modified' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        $events = $this->table('events');
+        $events->insert($data)
+            ->save();
+    }
+}
